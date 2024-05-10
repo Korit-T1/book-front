c0cf66a7dd584cb007437b3f43e0fcdea9a911fd
rnurphy <ajvlzla0207@gmail.com>
2024-05-10 17:36:56 +0900

    모달 디자인 및 기능 점검

--- src/components/BookDetailModal/style.js
+++ src/components/BookDetailModal/style.js
@@ -3,8 +3,9 @@ import { css } from "@emotion/react";
 export const container = css`
     box-sizing: border-box;
     display: flex;
+    justify-content: space-between;
     width: 100%;
-    height: 100%;
+    height: 625px;
 `
 export const boxButton = css`
     position: absolute;
@@ -12,10 +13,11 @@ export const boxButton = css`
 `
 export const imgBox = css`
     box-sizing: border-box;
+    border: 1px solid;
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 45%;
+    width: 49%;
     height: 100%;
     overflow: hidden;
     box-shadow: 0px 0px 4px #000000b6;
@@ -29,36 +31,117 @@ export const imgBox = css`
     }
 `
 export const infoBox = css`
-    border: 1px none;
-    box-sizing: border-box;
-    justify-content: center;
-    align-items: center;
-    width: 55%;
-    height: 100%;
-    margin-left: 20px;
+  /* border: 1px solid;
+  box-sizing: border-box; */
+  justify-content: center;
+  align-items: center;
+  width: 49%;
+  height: 100%;
 `
+
+export const btns = css`
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  box-sizing: border-box;
+  height: 40px;
+`
+
+export const btn = css`
+  width: 50%;
+  background-color: white;
+  border: none;
+  cursor: pointer;
+`
+
 export const bookInfo = css`
     border: 1px solid #dbdbdb;
-    padding: 0px 15px;
+    
+    /* padding: 0px 15px; */
     margin-bottom: 10px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+    /* text-overflow: ellipsis; */
+    /* white-space: nowrap; */
     overflow: hidden;
+    height: 200px;
+    & > h1 {
+      font-size: 40px;
+      margin: 0;
+      padding: 0;
+    }
 `
+
+export const bookRate = css`
+  border: 1px solid #dbdbdb;
+  margin-bottom: 10px;
+`
+
 export const stockInfo = css`
     border: 1px solid #dbdbdb;
-    margin-bottom: 10px;
+    height: 285px;
+
+    & > table, th, td {
+      border-collapse: collapse;
+      border: 1px solid black;
+      font-size: 20px;
+    }
+
+    th, td {
+      height: 50px;
+    }
+
+    td {
+      min-width: 100px;
+      text-align: center;
+    }
 `
-export const reviewArea = css`
-    width: 100%;
-    height: 120px;
-    resize: none;
-`
-export const Base = css`                            //styled.section
+export const summary = css`
   display: flex;
   align-items: center;
-  gap: 8px;
-`;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 80px;
+  /* box-sizing: border-box;
+  border: 1px solid; */
+
+  h2 {
+    margin: 0;
+  }
+
+  div:nth-of-type(2) {
+    display: flex;
+    /* justify-content: center; */
+    align-items: flex-end;
+
+    span:nth-of-type(1) {
+      font-size: 35px;
+      font-weight: bold;
+      margin-bottom: 6px;
+    }
+
+    span:nth-of-type(2) {
+      margin-left: 5px;
+      font-size: 20px;
+      font-weight: bold;
+      color: gray;
+      margin-bottom: 8.5px;
+    }
+  }
+`
+
+
+export const reviewArea = css`
+  box-sizing: border-box;
+  width: 100%;
+  height: 80px;
+  resize: none;
+  font-size: 18px;
+  border: 1px solid #dbdbdb;
+  :focus {
+    outline: none;
+  }
+`
+
 
 export const Name = css`                             //styled.span
   font-size: 1.4rem;
@@ -83,8 +166,121 @@ export const RatingField = css`                 //styled.fieldset
     color: orange;
   }
 `;
+
+export const reviewBox = css`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  border: 1px solid #dbdbdb;
+  height: 330px;
+
+  p, span {
+    margin: 0;
+  }
+`;
+
+export const reviewPages = css`
+  box-sizing: border-box;
+  border: 1px solid #dbdbdb;
+  height: 40px;
+`;
+
 export const reviewContent = css`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  
+  box-sizing: border-box;
+  border: 1px solid;
+  height: 20%;
+`;
+
+export const reviewContentLeft = css`
+  width: 415px;
+  p:nth-of-type(2) {
+    margin-left: 200px;
+  }
+  box-sizing: border-box;
+  border: 1px solid;
+`;
+
+export const reviewContentRight = css`
+  width: 75px;
+  box-sizing: border-box;
+  border: 1px solid;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  div:nth-of-type(1) {
+    margin-left: 5px;
+  }
+  div:nth-of-type(2) {
+    p {
+      padding-left: 5px;
+      font-size: 23px;
+    }
+  }
+`;
+
+export const bottom = css`
+  display: flex;
+  height: 130px;
+  box-sizing: border-box;
+  border: 1px solid;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+export const Base = css`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  /* box-sizing: border-box;
+  border: 1px solid;  */
+  height: 50px;
+
+  div:nth-of-type(2) {
+    height: 35px;
+    span:nth-of-type(1) {
+        font-weight: bold;
+        font-size: 30px;
+      } 
+    }
+
+    div:nth-of-type(1) {
+
+    }
+`;
+
+export const comment = css`
+/* box-sizing: border-box;
+  border: 1px solid; */
+    display: flex;
+    height: 80px;
 `
+
+export const submit = css`
+  width: 100px;
+  height: 35px;
+  background: none;
+  box-sizing: border-box;
+  border: 1px solid #dbdbdb;
+  cursor: pointer;
+
+  :hover {
+    background-color: #dbdbdb;
+  }
+`
+
+export const base1 = css`
+  width: 50%;
+`;
+
+export const base2 = css`
+  width: 25%;
+`;
+
+export const base3 = css`
+  width: 25%;
+`;
+
